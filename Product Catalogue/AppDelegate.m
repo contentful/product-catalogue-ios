9f7802a98275657b8aca74b0078d605f130a5511
Boris Bügling <boris@icculus.org>
2014-12-11 17:56:32 +0100

    Use cp-keys for space key and access token defaults.

--- Product Catalogue/AppDelegate.m
+++ Product Catalogue/AppDelegate.m
@@ -6,40 +6,35 @@
 //  Copyright (c) 2014 Boris Bügling. All rights reserved.
 //
 
+#import <CocoaPods-Keys/ProductCatalogueKeys.h>
+
 #import "AppDelegate.h"
+#import "Constants.h"
 
 @interface AppDelegate ()
 
 @end
 
+#pragma mark -
+
 @implementation AppDelegate
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self writeKeysToUserDefaults];
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
+-(void)writeKeysToUserDefaults {
+    NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+    ProductCatalogueKeys* keys = [ProductCatalogueKeys new];
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
+    if (![defaults stringForKey:SPACE_KEY]) {
+        [defaults setValue:keys.productCatalogueSpaceId forKey:SPACE_KEY];
+    }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    if (![defaults stringForKey:ACCESS_TOKEN]) {
+        [defaults setValue:keys.productCatalogueAccesToken forKey:ACCESS_TOKEN];
+    }
 }
 
 @end
---
+++ Product Catalogue/Constants.h
@@ -0,0 +1,10 @@
+//
+//  Constants.h
+//  Product Catalogue
+//
+//  Created by Boris Bügling on 11/12/14.
+//  Copyright (c) 2014 Boris Bügling. All rights reserved.
+//
+
+#define SPACE_KEY       @"ContentfulSpaceKey"
+#define ACCESS_TOKEN    @"ContentfulAccessToken"
