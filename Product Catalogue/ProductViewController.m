1d8afdcf91e52a0c8f0872dbbf5591d242acb655
Boris Bügling <boris@icculus.org>
2014-12-22 19:11:37 +0100

    Fill product view controller with actual data

--- Product Catalogue/ProductViewController.m
+++ Product Catalogue/ProductViewController.m
@@ -6,9 +6,22 @@
 //  Copyright (c) 2014 Boris Bügling. All rights reserved.
 //
 
+#import "Brand.h"
 #import "GalleryViewController.h"
 #import "ProductViewController.h"
 #import "StoryboardIdentifiers.h"
+
+@interface ProductViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *availabilityLabel;
+@property (weak, nonatomic) IBOutlet UIButton *brandButton;
+@property (weak, nonatomic) IBOutlet UIButton *buyButton;
+@property (weak, nonatomic) IBOutlet UILabel *pricingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *productNameLabel;
+
+@end
+
+#pragma mark -
 
 @implementation ProductViewController
 
@@ -19,6 +32,23 @@
         galleryVC.client = self.client;
         
     }
+}
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+
+    if (self.product.quantity.integerValue == 0) {
+        self.availabilityLabel.text = NSLocalizedString(@"No products\nin stock",
+                                                        @"Product quantity label");
+    } else if (self.product.quantity.integerValue == 1) {
+
+    } else {
+        self.availabilityLabel.text = [NSString stringWithFormat:NSLocalizedString(@"%@ items\nin stock", @"Product quantity label"), self.product.quantity];
+    }
+
+    [self.brandButton setTitle:[NSString stringWithFormat:NSLocalizedString(@"by %@", @"Brand button"), self.product.brand.companyName] forState:UIControlStateNormal];
+    self.pricingLabel.text = [NSString stringWithFormat:NSLocalizedString(@"%@\nEUR", @"Pricing label"), self.product.price];
+    self.productNameLabel.text = self.product.productName;
 }
 
 @end
