6daa6b9aec8208ebfd75025b06387336faff13b5
Boris Bügling <boris@icculus.org>
2015-01-22 17:37:32 +0100

    Verify presence of correct content types

---
+++ Product Catalogue/CMAProductCatalogueVerifier.h
@@ -0,0 +1,14 @@
+//
+//  CMAProductCatalogueVerifier.h
+//  Product Catalogue
+//
+//  Created by Boris Bügling on 22/01/15.
+//  Copyright (c) 2015 Boris Bügling. All rights reserved.
+//
+
+#import <ContentfulLogin/CMAAccessTokenSpaceVerifier.h>
+#import <Foundation/Foundation.h>
+
+@interface CMAProductCatalogueVerifier : CMAAccessTokenSpaceVerifier
+
+@end
